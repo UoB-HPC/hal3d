43371626daf19a37b49210dcd5e4398b4f97a984
Matt Martineau <mm14628@bristol.ac.uk>
2017-11-02 12:25:17 +0000

    Altered the calculation of profiling to perform some experimentation.

--- omp3/hale.c
+++ omp3/hale.c
@@ -30,12 +30,14 @@ void solve_unstructured_hydro_3d(Mesh* mesh, HaleData* hale_data,
   // Describe the subcell node layout
   printf("\nPerforming the Lagrangian Phase\n");
 
+  struct Profile out;
+
   // Perform the Lagrangian phase of the ALE algorithm where the mesh will
   // move
   // due to the pressure (ideal gas) and artificial viscous forces
-  START_PROFILING(&compute_profile);
+  START_PROFILING(&out);
   lagrangian_phase(mesh, umesh, hale_data);
-  STOP_PROFILING(&compute_profile, "Lagrangian phase");
+  STOP_PROFILING(&out, "Lagrangian phase");
 
   if (hale_data->visit_dump) {
     write_unstructured_to_visit_3d(umesh->nnodes, umesh->ncells, timestep * 2,
@@ -52,59 +54,48 @@ void solve_unstructured_hydro_3d(Mesh* mesh, HaleData* hale_data,
     double initial_ke_mass = 0.0;
     vec_t initial_momentum = {0.0, 0.0, 0.0};
 
-#if 0
-    //
-    //
-    //
-    //
-    mass_repair_phase(umesh, hale_data);
-    //
-    //
-    //
-    //
-    //
-#endif // if 0
-
     // gathers all of the subcell quantities on the mesh
-    START_PROFILING(&compute_profile);
+    START_PROFILING(&out);
     gather_subcell_quantities(umesh, hale_data, &initial_momentum,
                               &initial_mass, &initial_ie_mass,
                               &initial_ke_mass);
-    STOP_PROFILING(&compute_profile, "Gather phase");
+    STOP_PROFILING(&out, "Gather phase");
 
     printf("\nPerforming Advection Phase\n");
 
     // Performs a remap and some scattering of the subcell values
-    START_PROFILING(&compute_profile);
+    START_PROFILING(&out);
     advection_phase(umesh, hale_data);
-    STOP_PROFILING(&compute_profile, "Advection phase");
+    STOP_PROFILING(&out, "Advection phase");
 
     printf("\nPerforming Eulerian Mesh Rezone\n");
 
     // Performs an Eulerian rezone, returning the mesh and reconciling fluxes
-    START_PROFILING(&compute_profile);
+    START_PROFILING(&out);
     eulerian_rezone(umesh, hale_data);
-    STOP_PROFILING(&compute_profile, "Rezone phase");
+    STOP_PROFILING(&out, "Rezone phase");
 
     printf("\nPerforming Repair Phase\n");
 
     // Fixes any extrema introduced by the advection
-    START_PROFILING(&compute_profile);
+    START_PROFILING(&out);
     mass_repair_phase(umesh, hale_data);
-    STOP_PROFILING(&compute_profile, "Repair phase");
+    STOP_PROFILING(&out, "Repair phase");
     printf("\nPerforming the Scattering Phase\n");
 
     // Perform the scatter step of the ALE remapping algorithm
-    START_PROFILING(&compute_profile);
+    START_PROFILING(&out);
     scatter_phase(umesh, hale_data, &initial_momentum, initial_mass,
                   initial_ie_mass, initial_ke_mass);
-    STOP_PROFILING(&compute_profile, "Scatter phase");
+    STOP_PROFILING(&out, "Scatter phase");
 
     // Fixes any extrema introduced by the advection
-    START_PROFILING(&compute_profile);
+    START_PROFILING(&out);
     velocity_repair_phase(umesh, hale_data);
     energy_repair_phase(umesh, hale_data);
-    STOP_PROFILING(&compute_profile, "Repair phase");
+    STOP_PROFILING(&out, "Repair phase");
+
+    PRINT_PROFILING_RESULTS(&out);
 
 #if 0
     write_unstructured_to_visit_3d(umesh->nnodes, umesh->ncells, timestep * 2,
